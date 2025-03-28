84e9079e5a52e14cbe056be5cb1b0f8055dcaaf4
Akashkumar888 <akashgkr12@gmail.com>
2025-03-29 04:47:47 +0530

    update

--- Modern-Web-Development-project-1/script.js
+++ Modern-Web-Development-project-1/script.js
@@ -49,12 +49,14 @@ function firstPageAnim(){
         })    
 }
 
-firstPageAnim(); // Calling the function to start the animation
+
 
 
 
 
 // jab mouse move ho to hum log skew kr paye aur maximum skew and minimum skew define kr paaye , jb mouse move ho to chapta ki value bade, aur jb mouse chalna band ho jaye to chapta hata lo
+
+var timeout;
 
 function circleChaptaKaro(){
   var xPrev=0;
@@ -63,8 +65,8 @@ function circleChaptaKaro(){
   var yScale=1;
 
 
-   window.addEventListener("mousemove",{
-    function(dets){
+   window.addEventListener("mousemove",function(dets){
+    clearTimeout(timeout);
       var xDiff=dets.clientX-xPrev;
       var yDiff=dets.clientY-yPrev;
       xScale= gsap.utils.clamp(0.8,1.2,xDiff);
@@ -72,17 +74,58 @@ function circleChaptaKaro(){
       
       xPrev=dets.clientX;
       yPrev=dets.clientY;
-    }
-   })
+      akash(xScale,yScale);
+      timeout=setTimeout(function(){
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)   scale(1,1)`;
+      },100)
+
+   });
+}
+
+
+function akash(xScale,yScale){
+  window.addEventListener("mousemove", function(dets){
+    document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)   scale(${xScale},${yScale})`;
+  });
 }
 
 circleChaptaKaro();
-
-function akash(){
-  window.addEventListener("mousemove", function(dets){
-    document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
-  });
-}
 akash();
+firstPageAnim(); // Calling the function to start the animation
+
+// teeno element ko sleect karo, uske baad teeno par ek mousemove lagao, jab mousemove ho to ye pata karo ki mouse kaha par hai, jiska matlab hai mouse ki x and y position pata karo, ab mouse ki x y position ke badle us image ko show karo and us image ko move karo, move karte waqt rotate karo, and jaise jaise mouse tez chale waise waise rotation bhi tez ho jaye
+
+document.querySelectorAll(".elem").forEach(function(elem){
+ var rotate=0;
+ var diffrot=0;
+
+
+
+ elem.addEventListener('mouseleave',function(){
+    gsap.to(elem.querySelector("img"),{
+      opacity:0,
+      visibility: "hidden", // Show image
+      ease:Power3,
+      duration:0.5
+    });
+   });
+
+
+ elem.addEventListener('mousemove',function(dets){
+var diff=dets.clientY - elem.getBoundingClientRect().top;
+  diffrot=dets.clientX-rotate;
+  rotate=dets.clientX;
+
+  
+  gsap.to(elem.querySelector("img"),{
+    opacity:1,
+    visibility: "visible", // Show image
+    ease:Power3,
+    top:diff,
+    left:dets.clientX,
+    rotate:gsap.utils.clamp(-20,20,diffrot*0.5)
+  });
+ });
+});
 
 
